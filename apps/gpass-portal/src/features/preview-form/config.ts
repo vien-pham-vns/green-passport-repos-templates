8d2b2485ea68ca467a1dd8779d380e38841a3b32
Leo Huynh <phat.huynh@vnsilicon.net>
2026-01-09 14:40:27 +0700

    feat: preview form page

---
+++ apps/gpass-portal/src/features/preview-form/config.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_IFRAME_URL =
+  'https://egap.dt-dev.vnsilicon.cloud/central-lab/applications/30127fbd-1b5c-4f0e-9130-94351afea236';
+
+export const DEFAULT_MODAL_TITLE = 'Application Details';
